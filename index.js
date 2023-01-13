ef5abc2cd878d92643bd90f0a706672031a9aa99
Nicholas Caputo <nick.caputo12@gmail.com>
2023-01-12 20:39:14 -0800

    commit tree 7028876d5b63897d5b9ff8c2b18174844b172061

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+const employee = {
+    name: "Bob", 
+    streetAddress : "200 Broadway"
+}
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+return {...employee, [key]: value,} 
+}
+const newEmployee = updateEmployeeWithKeyAndValue (
+    employee, 
+    "Joe",
+    "500 Grand",
+)
+function deleteEmployeeByKey (emplyee, key, value) {
+    return (delete emplyee.name)
+}
